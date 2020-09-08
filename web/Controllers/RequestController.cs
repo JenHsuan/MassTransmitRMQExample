2fe4170f46ae45c77cc22f16e832f67b4e60f938
Sean Hsieh <sean.hsieh@garmin.com>
2020-09-08 13:56:00 +0800

    Add the controller
    Change-Id: I3fa775055cd5ddc5cf4d40ec5b15396ad322b9da

---
+++ web/Controllers/RequestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using web.Contracts;
+
+namespace web.Controllers
+{
+    [Produces("application/json")]
+    [Route("[controller]")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        private readonly IRequestClient<CheckOrderStatus> _checkStatusClient;
+        public RequestController(IRequestClient<CheckOrderStatus> checkStatusClient)
+        {
+            _checkStatusClient = checkStatusClient;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var response = await _checkStatusClient.GetResponse<OrderStatusResult>(new { OrderId = id });
+            return Ok(response.Message);
+        }
+    }
+}
