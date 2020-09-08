4d73b9905709ee91975d879c261531fe312c5931
Sean Hsieh <sean.hsieh@garmin.com>
2020-09-08 13:54:54 +0800

    Add the RMQ bus
    Change-Id: I365675564cc105310e95368b40f5e70fc702bf60

---
+++ web/Configuration/RabbitMq.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web.Configuration
+{
+    public class RabbitMq
+    {
+        public string Host { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ web/Configuration/RabbitMqBus.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using MassTransit.RabbitMqTransport;
+using Microsoft.Extensions.DependencyInjection;
+using web.Observers;
+
+namespace web.Configuration
+{
+    public class RabbitMqBus
+    {
+        public static IBusControl CreateRabbitMqBus(IRegistrationContext<IServiceProvider> context)
+        {
+            var rabbitMq = context.Container.GetRequiredService<RabbitMq>();
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host(rabbitMq.Host, hostConfigurator =>
+                {
+                    hostConfigurator.Username(rabbitMq.Username);
+                    hostConfigurator.Password(rabbitMq.Password);
+                });
+                //cfg.UseServiceBusMessageScheduler();
+                cfg.UseDelayedExchangeMessageScheduler();
+                cfg.ConfigureEndpoints(context);
+                //Configure endpoints automatically. Might need to configure them manually, if we want to set up custom retry / redelivery stuff.
+
+                cfg.ConnectBusObserver(context.Container.GetRequiredService<BusObserver>());
+            });
+
+            bus.ConnectSendObserver(context.Container.GetRequiredService<SendObserver>());
+            bus.ConnectReceiveObserver(context.Container.GetRequiredService<ReceiveObserver>());
+
+            return bus;
+        }
+    }
+}
